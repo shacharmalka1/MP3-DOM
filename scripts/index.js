5a20e30a953f4f9d3053d080586b752eb15b75e5
shacharmalka1 <shacharmalka0204@gmail.com>
2021-09-12 23:31:56 +0300

    works

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@
  * @param {String} songId - the ID of the song to play
  */
 function playSong(songId) {
-    // Your code here
+    alert("works")
 }
 
 /**
@@ -28,7 +28,11 @@ function createSongElement({ id, title, album, artist, duration, coverArt }) {
  * Creates a playlist DOM element based on a playlist object.
  */
 function createPlaylistElement({ id, name, songs }) {
-    const children = []
+    const children = [
+        createElement("p", name, [], {}),
+        createElement("p", songs.length, [], {}),
+        createElement("p", playlistDuration(id), [], {}),
+    ]
     const classes = []
     const attrs = {}
     return createElement("div", children, classes, attrs)
@@ -40,29 +44,32 @@ function createPlaylistElement({ id, name, songs }) {
  * Example usage:
  * createElement("div", ["just text", createElement(...)], ["nana", "banana"], {id: "bla"})
  *
- * @param {String} tagName - the type of the element
- * @param {Array} children - the child elements for the new element.
+ * @param {String} tagName - the type of the element: for example- p,h1,div...
+ * @param {Array} children - the child elements for the new element: for example- assume ew have <ul> his children <li>
  *                           Each child can be a DOM element, or a string (if you just want a text element).
  * @param {Array} classes - the class list of the new element
  * @param {Object} attributes - the attributes for the new element
  */
 function createElement(tagName, children = [], classes = [], attributes = {}) {
     const element = document.createElement(tagName)
-    for (let child of children) {
-        element.append(child)
+    if (Array.isArray(children))
+        for (let child of children) {
+            element.append(child)
+        }
+    else {
+        element.innerHTML = children
     }
     for (let name of classes) {
-        element.classList.append(name)
+        element.classList.add(name)
     }
-    for (let attribute of attributes) {
+    for (let attribute in attributes) {
         element.setAttribute(attribute, attributes[attribute])
     }
     return element
 }
 
-const elemntHtml = document.getElementById("songs")
-
 // You can write more code below this line
+
 function durationFormat(duration) {
     //converting from seconds to mm:ss format
     let minutes = Math.floor(duration / 60)
@@ -71,4 +78,45 @@ function durationFormat(duration) {
     else if (minutes < 10) return "0" + minutes + ":" + seconds
     else if (seconds < 10) return minutes + ":" + "0" + seconds
     else return minutes + ":" + seconds
+}
+
+function playlistDuration(id) {
+    let correctPlaylist = findPlaylistById(id) //correctPlaylist contain the wanted playlist
+    let saveSongId = 0,
+        sum = 0
+    for (let i = 0; i < correctPlaylist.songs.length; i++) {
+        //run on the songs array inside this playlist
+        saveSongId = correctPlaylist.songs[i]
+        for (let j = 0; j < player.songs.length; j++) {
+            //run on the songs array
+            if (player.songs[j].id === saveSongId) sum += player.songs[j].duration
+        }
+    }
+    return durationFormat(sum)
+}
+
+function findPlaylistById(id) {
+    //Get a playlist id and return the wanted playlist by his id
+    let correctPlaylist
+    for (let i = 0; i < player.playlists.length; i++) {
+        //run on playlists array
+        if (id === player.playlists[i].id) correctPlaylist = player.playlists[i]
+    }
+    return correctPlaylist
+}
+
+const elemHtml = document.getElementById("songs")
+const elemHtml1 = document.getElementById("playlists")
+
+player.playlists.sort((playlistA, playlistB) => playlistA.name.localeCompare(playlistB.name))
+player.songs.sort((songA, songB) => songA.title.localeCompare(songB.title))
+
+for (let i = 0; i < player.songs.length; i++) {
+    //songs
+    elemHtml.append(createSongElement(player.songs[i]))
+}
+
+for (let i = 0; i < player.playlists.length; i++) {
+    //playlists
+    elemHtml1.append(createPlaylistElement(player.playlists[i]))
 }
